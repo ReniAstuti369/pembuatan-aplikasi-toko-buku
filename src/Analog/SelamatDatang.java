652bebd547ec3966f58563ead57eebb01d6e3ec4
User <User@LAPTOP-DHSKS670>
2020-06-23 14:45:28 +0700

    Toko Buku

---
+++ src/Analog/SelamatDatang.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Analog;
+
+import Perpustakaan.Login;
+import javax.swing.Box;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author User
+ */
+public class SelamatDatang {
+    public static void main(String[]args){
+        //menampilkan informasi
+        JOptionPane.showMessageDialog(null, " Nama     : Reni Astuti \n Jurusan : Sistem Komputer");
+        new Buku().setVisible(true);
+    }
+
+    }
+
+
+            
+            
+            
+        
+        
+
+
+
